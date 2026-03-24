2c0b638c04dbe210765f9b4c252c7b3e71919bfa
Daniel Copete <dcopjim3009g.educaand.es>
2026-03-24 09:24:22 +0100

    Añadiendo Ejercicio 1 de la prueba de ENDES

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DanielCopete_Ej1-Pru_UD4
+En este repositorio de GitHub voy alojar el ejercicio 1 de la prueba completa de la unidad 4 de entornos de desarrollo
---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
